d8b87074ac3b91c62e07eeb41f66385a3718238a
Owen McComas <owen@mccomas.me>
2023-01-10 20:56:45 -0700

    Instructions completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,28 @@
-import React, { useState, useEffect } from 'react'
-import Friend from './Friend'
-import FriendForm from './FriendForm'
-import axios from '../axios'
+import React, { useState, useEffect } from "react";
+import Friend from "./Friend";
+import FriendForm from "./FriendForm";
+import axios from "../axios";
 
 // 👉 the shape of the state that drives the form
 const initialFormValues = {
   ///// TEXT INPUTS /////
-  username: '',
-  email: '',
+  username: "",
+  email: "",
   ///// DROPDOWN /////
-  role: '',
-}
+  role: "",
+};
 
 export default function App() {
-  const [friends, setFriends] = useState([]) // careful what you initialize your state to
+  const [friends, setFriends] = useState([]); // careful what you initialize your state to
 
   // 🔥 STEP 1 - WE NEED STATE TO HOLD ALL VALUES OF THE FORM!
-  const formValues = {} // fix this using the state hook
+  const [formValues, setFormValues] = useState(initialFormValues); // fix this using the state hook
 
   const updateForm = (inputName, inputValue) => {
     // 🔥 STEP 8 - IMPLEMENT a "form state updater" which will be used inside the inputs' `onChange` handler
     //  It takes in the name of an input and its value, and updates `formValues`
-  }
+    setFormValues({...formValues, [inputName]: inputValue})
+  };
 
   const submitForm = () => {
     // 🔥 STEP 9 - IMPLEMENT a submit function which will be used inside the form's own `onSubmit`
@@ -29,30 +30,41 @@ export default function App() {
     //  b) prevent further action if either username or email or role is empty string after trimming
     //  c) POST new friend to backend, and on success update the list of friends in state with the new friend from API
     //  d) also on success clear the form
+  
+    const newFriend = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role
+    }
+
+    if(!newFriend.username || !newFriend.email || !newFriend.role) return;
+
+    axios.post('fakeapi.com', newFriend)
+      .then(res => {console.log(res)})
+      setFormValues(initialFormValues)
+      .catch(console.error(err))
   }
 
   useEffect(() => {
-    axios.get('fakeapi.com').then(res => setFriends(res.data))
-  }, [])
+    axios.get("fakeapi.com").then((res) => setFriends(res.data));
+  }, []);
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Form App</h1>
 
       <FriendForm
         // 🔥 STEP 2 - The form component needs its props.
         //  Check implementation of FriendForm
         //  to see what props it expects.
-        values={{}}
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
       />
 
-      {
-        friends.map(friend => {
-          return (
-            <Friend key={friend.id} details={friend} />
-          )
-        })
-      }
+      {friends.map((friend) => {
+        return <Friend key={friend.id} details={friend} />;
+      })}
     </div>
-  )
-}
+  );
+    }
--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -1,50 +1,80 @@
-import React from 'react'
+import React from "react";
 
 export default function FriendForm(props) {
   // THESE ARE THE **EXACT PROPS** FriendForm EXPECTS!!!
-  const { values, update, submit } = props
+  const { values, update, submit } = props;
 
-  const onChange = evt => {
+  const onChange = (evt) => {
     // 🔥 STEP 6 - IMPLEMENT the change handler for our inputs and dropdown
     // a) pull the name of the input from the event object
+    const name = evt.target.name;
     // b) pull the value of the input from the event object
+    const { value } = evt.target;
     // c) use the `update` callback coming in through props
-  }
+    update(name, value);
+  };
 
-  const onSubmit = evt => {
+  const onSubmit = (evt) => {
     // 🔥 STEP 7 - IMPLEMENT the submit handler
     // a) don't allow the browser to reload!
+    evt.preventDefault();
     // c) use the `submit` callback coming in through props
-  }
+    submit()
+  };
 
   return (
-    <form className='form container' onSubmit={onSubmit}>
-      <div className='form-group inputs'>
+    <form className="form container" onSubmit={onSubmit}>
+      <div className="form-group inputs">
         {/* ////////// TEXT INPUTS ////////// */}
         {/* ////////// TEXT INPUTS ////////// */}
         {/* ////////// TEXT INPUTS ////////// */}
-        <label>Username
+        <label>
+          Username
           {/* 🔥 STEP 3 - Make an input of type `text` for username.
               Controlled inputs need `value` and `onChange` props.
               Inputs render what they're told - their current value comes from app state.
               At each keystroke, a change handler fires to change app state. */}
         </label>
 
-        <label>Email
+        <input
+          type="text"
+          name="username"
+          placeholder="Type Username"
+          value={values.username}
+          onChange={onChange}
+          maxLength="15"
+        />
+
+        <label>
+          Email
           {/* 🔥 STEP 4 - Make an input of type `email` or `text` for email. */}
         </label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Type Email"
+          value={values.email}
+          onChange={onChange}
+        />
 
         {/* ////////// DROPDOWN ////////// */}
         {/* ////////// DROPDOWN ////////// */}
         {/* ////////// DROPDOWN ////////// */}
-        <label>Role
+        <label>
+          Role
           {/* 🔥 STEP 5 - Make dropdown for role. */}
+          <select value={values.role} name="role" onChange={onChange}>
+            <option value="">--- Select a Role---</option>
+            <option value="Student">Student</option>
+            <option value="Instructor">Instructor</option>
+            <option value="Alumni">Alumni</option>
+          </select>
         </label>
 
-        <div className='submit'>
+        <div className="submit">
           <button>submit</button>
         </div>
       </div>
     </form>
-  )
+  );
 }
